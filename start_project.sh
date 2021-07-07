69d728c62feb414651f239a9f22fafc1308b42e6
Randy Fay <randy@randyfay.com>
2021-07-07 12:46:53 -0600

    Make files in container

--- start_project.sh
+++ start_project.sh
@@ -3,4 +3,4 @@
 set -eu -o pipefail
 
 docker rm -f tester99 2>/dev/null || true
-docker run --name=tester99 --rm -it --detach busybox tail -f /dev/null
+docker run --name=tester99 --rm -it --detach busybox sh -c "touch /var/www/junk.txt /var/spool/junk2.txt && tail -f /dev/null"
