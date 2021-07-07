7b28e898c2c8411f478a7a7629b376e3a30a4550
Randy Fay <randy@randyfay.com>
2021-07-07 11:48:50 -0600

    Should not mount anything

--- start_project.sh
+++ start_project.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x
 
 docker rm -f tester99 || true
-# Mount PWD into /var, which doesn't have much useful on busybox, just spool and www
-docker run --name=tester99 --rm -it -v $PWD:/var busybox sh
+docker run --name=tester99 --rm -it --detach busybox tail -f /dev/null
