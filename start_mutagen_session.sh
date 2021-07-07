429b8ae822c6ff42db6fbe5812b19b89b77e1046
Randy Fay <randy@randyfay.com>
2021-07-07 11:50:26 -0600

    script improvements

--- start_mutagen_session.sh
+++ start_mutagen_session.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
 
+set -eu -o pipefail
+
 mutagen daemon start
 mutagen sync create $PWD docker://tester99/var --sync-mode=two-way-resolved --name=tester99
--- start_project.sh
+++ start_project.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
 
-docker rm -f tester99 || true
+set -eu -o pipefail
+
+docker rm -f tester99 2>/dev/null || true
 docker run --name=tester99 --rm -it --detach busybox tail -f /dev/null
