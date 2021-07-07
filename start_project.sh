67c827d36bc047139d78c5f8b4f0978b82f716b1
Randy Fay <randy@randyfay.com>
2021-07-07 11:43:40 -0600

    Kill off container before starting

--- start_project.sh
+++ start_project.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
+docker rm -f tester99 || true
 # Mount PWD into /var, which doesn't have much useful on busybox, just spool and www
 docker run --name=tester99 --rm -it -v $PWD:/var busybox sh
